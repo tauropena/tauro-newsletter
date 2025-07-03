9a4ae5579250ec6c5a25f11ce52bc2c77c64cbae
Amrita Sawhney <amritak.saw@gmail.com>
2025-07-02 23:14:03 -0400

    login page styling

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -39,23 +39,21 @@ export default function Login() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-2xl text-black font-bold mb-6 text-left">
+    <div className="min-h-screen flex items-center justify-center bg-[#26211D]">
+      <div className="p-8 rounded w-96">
+        <h1 className="text-2xl text-[#DCD6c6] font-bold mb-6 text-left">
           who is the prettiest girl in the whole wide world?
         </h1>
 
         {error && (
-          <div className="mb-4 p-2 bg-red-100 text-red-700 rounded">
-            {error}
-          </div>
+          <div className="mb-4 p-2 text-[#EF738C] rounded">{error}</div>
         )}
 
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               htmlFor="password"
-              className="block text-sm font-medium text-gray-700 mb-1"
+              className="block text-sm font-medium text-gray-400 mb-1"
             >
               password
             </label>
@@ -65,7 +63,7 @@ export default function Login() {
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full text-black px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full text-white px-3 py-2 border border-[#FCDC59] bg-[#3A3631] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
                 disabled={loading}
               />
@@ -78,41 +76,44 @@ export default function Login() {
               </button>
             </div>
           </div>
-          <button
-            type="submit"
-            disabled={loading}
-            className={`w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition" ${
-              loading ? "opacity-70 cursor-not-allowed" : ""
-            }`}
-          >
-            {loading ? (
-              <span className="flex items-center justify-center">
-                <svg
-                  className="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                prepare to enter...
-              </span>
-            ) : (
-              "sign in"
-            )}
-          </button>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-auto px-6 bg-[#DCD6C6] text-[#26211D] py-2 px-4 rounded-full border-2 border-[#26211D] 
+                hover:translate-y-[-0.25rem] hover:shadow-[0_0.25rem_0_0_#C8C2B2] transition" ${
+                  loading ? "opacity-70 cursor-not-allowed" : ""
+                }`}
+            >
+              {loading ? (
+                <span className="flex items-center justify-center">
+                  {/* <svg
+                    className="animate-spin -ml-1 mr-2 h-4 w-4 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg> */}
+                  prepare to enter...
+                </span>
+              ) : (
+                "sign in"
+              )}
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -120,7 +121,6 @@ export default function Login() {
 }
 
 /* 
-change the button styling to match the mailchimp styling
 workshop the text to be more clear and communicative for users
 add picture of coco
 show and hide to eye shut and eye open
