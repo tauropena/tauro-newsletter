6352c75b2eff63b41a4eb772e758d4205092a6a6
tauropena <somopena@gmail.com>
2025-10-16 19:57:44 -0400

    changed auth button

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      <h1 className=" text-gray-400 mb-4">are you lost?</h1>
+      <h1 className=" text-foreground mb-4">hey cutie</h1>
       <button
         onClick={() => router.push("/login")}
         className="cursor-pointer px-6 text-2xl bg-foreground text-background py-2 rounded-full border-2 border-background 
         hover:translate-y-[-0.25rem] hover:shadow-[0_0.25rem_0_0_#C8C2B2] transition duration-300"
       >
-        click now
+        click here
       </button>
     </div>
   )
